84aeb7444bfd3ebf8f55dcab420ffbfbd7ba5625
Kamil Doğan <kamildogan@outlook.com>
2023-05-16 05:02:51 +0100

    add timeout for database connection

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-connectDB();
+setTimeout(() => {
+  connectDB();
+}, 3000);
 // Define a port number
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || ''
